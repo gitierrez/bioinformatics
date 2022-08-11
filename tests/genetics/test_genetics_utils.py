92b80418bd2fbfa8892b06b528f8feec81e223e5
Juan Gutierrez <jgutierrezh0@gmail.com>
2022-08-11 18:15:42 -0500

    Update unit tests

--- tests/genetics/test_genetics_utils.py
+++ tests/genetics/test_genetics_utils.py
@@ -1,21 +1,35 @@
 import pytest
-from biomics.genetics.utils import group_values_by_complement
+
+from biomics.genetics.utils import (
+    validate_nucleotides,
+    get_region_in_circular_array,
+    get_frequency_map,
+)
 
 
-@pytest.mark.skip(reason='Refactoring in process')
-def test_group_values_by_complement():
-    test_case = {
-        "AGCAG": 10,
-        "GCATT": 8,
-        "CTGCT": 9,
-        "AAAAA": 5,
-        "TTTTT": 7,
-        "CCCCC": 6,
-    }
-    expected_result = {
-        "AGCAG": 19,
-        "GCATT": 8,
-        "AAAAA": 12,
-        "CCCCC": 6,
-    }
-    assert group_values_by_complement(test_case) == expected_result
+def test_validate_nucleotides_with_valid_string():
+    assert validate_nucleotides('ATCG')
+
+
+def test_validate_nucleotides_with_invalid_string():
+    with pytest.raises(ValueError):
+        validate_nucleotides('ATCGX')
+
+
+@pytest.mark.parametrize(
+    'array, center, window_size, expected',
+    [
+        ('CATGGGCATCGGCCATACGCC', 8, 10, 'GGGCATCGGCC'),
+        ('CATGGGCATCGGCCATACGCC', 2, 10, 'GCCCATGGGCA'),
+    ],
+)
+def test_get_region_in_circular_array(array, center, window_size, expected):
+    assert get_region_in_circular_array(array, center, window_size) == expected
+
+
+def test_frequency_map():
+    freqs = get_frequency_map('ACGTTGCATGTCGCATGATGCATGAGAGCT', subsequence_length=4)
+    assert freqs["GCAT"] == 3
+    assert freqs["CATG"] == 3
+    assert freqs["ATGA"] == 2
+    assert freqs["TCGC"] == 1

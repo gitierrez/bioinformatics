c9c3211c1029b52c60ce3f6e10ebf1d3aeb3de38
Juan Gutierrez <jgutierrezh0@gmail.com>
2022-08-11 18:16:58 -0500

    Refactor genetics utils

--- biomics/genetics/utils.py
+++ biomics/genetics/utils.py
@@ -1,4 +1,4 @@
-from biomics.genetics import DNASequence
+from biomics.genetics.sequence import variations
 
 
 def validate_nucleotides(value: str, valid_nucleotides: set = None):
@@ -9,6 +9,7 @@ def validate_nucleotides(value: str, valid_nucleotides: set = None):
             raise ValueError(
                 f"Invalid character {nucleotide} found at position {i} in DNA sequence"
             )
+    return True
 
 
 def get_region_in_circular_array(array, center, window_size):
@@ -18,29 +19,14 @@ def get_region_in_circular_array(array, center, window_size):
     return array[-(half_window - center):] + array[: center + half_window + 1]
 
 
-def get_frequency_map(sequence: DNASequence, subsequence_length: int, **kwargs):
+def get_frequency_map(sequence: str, subsequence_length: int, **kwargs):
     frequency_map = {}
     for i in range(len(sequence) - subsequence_length + 1):
-        subsequence = DNASequence(sequence[i: i + subsequence_length])
-        similar_sequences = subsequence.variations(**kwargs)
+        subsequence = sequence[i: i + subsequence_length]
+        similar_sequences = variations(subsequence, **kwargs)
         for seq in similar_sequences:
             if seq in frequency_map:
                 frequency_map[seq] += 1
             else:
                 frequency_map[seq] = 1
     return frequency_map
-
-
-def group_values_by_complement(counts: dict) -> dict:
-    """
-    Group the values of dictionary by their complementary strand.
-    """
-    exclusions = set([])
-    grouped_counts = {}
-    for seq, count in counts.items():
-        if seq in exclusions:
-            continue
-        complement_seq = complement(seq)
-        grouped_counts[seq] = count + counts.get(complement_seq, 0)
-        exclusions.add(complement_seq)
-    return grouped_counts

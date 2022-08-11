efc83fde64d4bb7f61bcafe7d11c9999e2226489
Juan Gutierrez <jgutierrezh0@gmail.com>
2022-08-11 18:15:31 -0500

    Change DNASequence into string functions

--- biomics/genetics/sequence.py
+++ biomics/genetics/sequence.py
@@ -1,53 +1,54 @@
-from biomics.genetics.utils import validate_nucleotides
+def hamming_distance(p: str, q: str):
+    return sum(ch1 != ch2 for ch1, ch2 in zip(p, q))
 
 
-class DNASequence:
+def complement(sequence: str) -> str:
+    pairings = {"A": "T", "T": "A", "C": "G", "G": "C"}
+    return "".join(pairings[nucleotide] for nucleotide in sequence[::-1])
 
-    def __init__(self, value: str, validate: bool = True):
-        if validate:
-            validate_nucleotides(value, valid_nucleotides={'A', 'C', 'G', 'T'})
-        self.value = value
-        self._complement = None
 
-    @property
-    def complement(self):
-        if self._complement is None:
-            self._complement = DNASequence(self._get_complement(self.value))
-        return self._complement
+def mutations(sequence: str, max_mutations: int = 1) -> set[str]:
+    """
+    Returns all mutations of the sequence up to max_mutations.
 
-    def mutations(self, max_mutations: int = 1):
-        """
-        Returns all mutations of the sequence up to max_mutations.
+    For example, mutations('AT', 1) will return
+    ['AT', 'AC', 'AG', 'AA', 'CT', 'GT', 'TT']
+    """
+    bases = {"A", "C", "G", "T"}
+    if max_mutations == 0:
+        return {sequence}
+    if len(sequence) == 1:
+        return bases
+    mutations_ = set([])
+    suffix_mutations = mutations(sequence[1:], max_mutations=max_mutations)
+    for mutation in suffix_mutations:
+        if hamming_distance(sequence[1:], mutation) < max_mutations:
+            for base in bases:
+                mutations_.add(base + mutation)
+        else:
+            mutations_.add(sequence[0] + mutation)
+    return mutations_
 
-        For example, if the sequence is 'AT', mutations(1) will return
-        ['AT', 'AC', 'AG', 'AA', 'CT', 'GT', 'TT']
-        """
-        bases = ['A', 'C', 'G', 'T']
-        if max_mutations == 0:
-            return [self]
-        if len()
 
-    def variations(self, include_complement: bool = False, include_variations_up_to: int = 0):
-        """
-        Returns all variations of the sequence up to include_variations_up_to.
-        """
-        variations = {self}
-        if include_complement:
-            variations.add(self.complement)
-        if include_variations_up_to > 0:
-            for mutation in self.mutations(max_mutations=include_variations_up_to):
-                variations.add(mutation)
-            for mutation in self.complement.mutations(max_mutations=include_variations_up_to):
-                variations.add(mutation)
-        return variations
+# test when I want variations of only the base sequence, i.e. include_complement=False
+def variations(
+    sequence: str, include_complement: bool = False, include_variations_up_to: int = 0
+) -> set[str]:
+    """
+    Returns all variations of the sequence up to include_variations_up_to.
+    """
+    variations = {sequence}
 
-    @staticmethod
-    def _get_complement(value: str) -> str:
-        pairings = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
-        return ''.join(pairings[nucleotide] for nucleotide in value[::-1])
-
-    def __len__(self):
-        return len(self.value)
-
-    def __getitem__(self, item):
-        return self.value[item]
+    # add mutations of original sequence
+    for mutation in mutations(sequence, max_mutations=include_variations_up_to):
+        variations.add(mutation)
+    if include_complement:
+        # add complement of original sequence
+        complement_seq = complement(sequence)
+        variations.add(complement_seq)
+        # add variations of complement
+        for mutation in mutations(
+            complement_seq, max_mutations=include_variations_up_to
+        ):
+            variations.add(mutation)
+    return variations

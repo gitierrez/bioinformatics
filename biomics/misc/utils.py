4d9537fff12e7c4eb59003cec19e28e3ed1caa4f
Juan Gutierrez <jgutierrezh0@gmail.com>
2022-08-11 17:52:16 -0500

    Remove deprecated modules

--- biomics/misc/utils.py
+++
@@ -1,5 +0,0 @@
-def sort_dict(d: dict, ascending: bool = False) -> dict:
-    """
-    Sort a dictionary by its values.
-    """
-    return {k: v for k, v in sorted(d.items(), key=lambda x: x[1], reverse=not ascending)}
--- biomics/string/distance.py
+++
@@ -1,5 +0,0 @@
-def hamming_distance(p: str, q: str):
-    """
-    Compute the Hamming distance between two strings.
-    """
-    return sum(ch1 != ch2 for ch1, ch2 in zip(p, q))

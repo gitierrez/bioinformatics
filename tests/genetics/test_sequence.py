03a1816d7f3846f7ceb707b7a2f5f08a4604a176
Juan Gutierrez <jgutierrezh0@gmail.com>
2022-08-11 18:16:44 -0500

    Add unit tests to cover sequence.py functions

--- tests/genetics/test_sequence.py
+++ tests/genetics/test_sequence.py
@@ -1,13 +1,45 @@
 import pytest
 
-from biomics.genetics import DNASequence
+from biomics.genetics.sequence import (
+    hamming_distance,
+    complement,
+    mutations,
+    variations,
+)
 
 
-def test_instantiation_fails_with_invalid_sequence():
-    with pytest.raises(ValueError):
-        DNASequence('ACGUACGT')
+def test_hamming_distance():
+    assert hamming_distance('GGGCCGTTGGT', 'GGACCGTTGAC') == 3
+    assert hamming_distance('ACGT', 'ACGT') == 0
 
 
 def test_complement():
-    seq = DNASequence('ATCG')
-    assert seq.complement.value == 'CGAT'
+    assert complement('ATCG') == 'CGAT'
+
+
+@pytest.mark.parametrize(
+    'sequence, max_mutations, expected',
+    [
+        ('AT', 1, {'AT', 'AC', 'AG', 'AA', 'CT', 'GT', 'TT'}),
+        ('AT', 0, {'AT'}),
+        ('AT', 2, {
+            'AT', 'AC', 'AG', 'AA', 'CT', 'GT', 'TT', 'CA',
+            'CG', 'CC', 'GA', 'GG', 'GC', 'TA', 'TG', 'TC',
+        }),
+    ]
+)
+def test_mutations(sequence, max_mutations, expected):
+    assert mutations(sequence, max_mutations) == expected
+
+
+@pytest.mark.parametrize(
+    'sequence, include_complement, include_variations_up_to, expected',
+    [
+        ('GTATG', False, 0, {'GTATG'}),
+        ('GTATG', True, 0, {'GTATG', 'CATAC'}),
+        ('TT', False, 1, {'TT', 'TC', 'TG', 'TA', 'CT', 'GT', 'AT'}),
+        ('TT', True, 1, {'TT', 'TC', 'TG', 'TA', 'CT', 'GT', 'AT', 'AA', 'AC', 'AG', 'CA', 'GA'}),
+    ]
+)
+def test_variations(sequence, include_complement, include_variations_up_to, expected):
+    assert variations(sequence, include_complement, include_variations_up_to) == expected
